8947b13c16388c2bca8f6d226e915f64c2b14410
KassirHaidar <haidarkassir313@gmail.com>
2025-11-02 12:50:47 +0200

    add some documentation

--- scripts/forcast.js
+++ scripts/forcast.js
@@ -4,7 +4,11 @@ const getcityid = async (city) => {
   const base = "https://dataservice.accuweather.com/locations/v1/cities/search";
   const query = `?apikey=${key}&q=${city}`;
     const response = await fetch(base + query );
+    /**
+     * returns an array, sorted by the closest match
+     */
     const data = await response.json();
+    /*return the closest match */
     return data[0].Key;
 };
 
@@ -13,6 +17,9 @@ const getforcast = async (id) => {
     const base = "https://dataservice.accuweather.com/currentconditions/v1/";   
     const query = `${id}?apikey=${key}`;
     const response = await fetch(base + query);
+    /**
+     * returns an array with current conditions object of one element
+     */
     const data = await response.json();
     return data[0];
 }
